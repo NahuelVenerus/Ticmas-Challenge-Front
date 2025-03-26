2ef0a68f63e86a1f87e09d0d1a83526448ad1388
Nahuel Venerus <nahuelvenerus@gmail.com>
2025-03-26 10:37:37 -0300

    more deploy changes

--- services/signup.ts
+++ services/signup.ts
@@ -3,15 +3,15 @@ import { UserSignupDTO } from "@/src/DTOs/userSignupDTO";
 import axios, { AxiosError } from "axios";
 import { login } from "./login";
 
-export const signup = async (signupFormData: UserSignupDTO): Promise<ResponseObject<string>> => {  
-  if(!signupFormData.name || !signupFormData.lastname || !signupFormData.email || !signupFormData.password || !signupFormData.confirmPassword) {
-    return {success: false, data: "Debe ingresar todos los datos para crear su cuenta"}
+export const signup = async (signupFormData: UserSignupDTO): Promise<ResponseObject<string>> => {
+  if (!signupFormData.name || !signupFormData.lastname || !signupFormData.email || !signupFormData.password || !signupFormData.confirmPassword) {
+    return { success: false, data: "Debe ingresar todos los datos para crear su cuenta" }
   }
   try {
     const response: ResponseObject<string> = await axios.post(`${process.env.NEXT_PUBLIC_API_BASE_URL}/users/create`, signupFormData);
 
-    await login({email: signupFormData.email, password: signupFormData.password});
-    
+    await login({ email: signupFormData.email, password: signupFormData.password });
+
     return {
       success: true,
       data: response.data
@@ -19,6 +19,12 @@ export const signup = async (signupFormData: UserSignupDTO): Promise<ResponseObj
 
   } catch (error: unknown) {
     if (error instanceof AxiosError) {
+      if (error.response?.status === 409) {
+        return {
+          success: false,
+          data: "Ya existe una cuenta con esa dirección de correo electrónico. Por favor, intenta con otra o accede a tu cuenta si ya tienes una."
+        };
+      }
       return {
         success: false,
         data: "No se pudo crear la cuenta debido a un problema con el servidor. Inténtelo nuevamente más tarde."
